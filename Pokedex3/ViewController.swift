bfdf1ed81b21c43fd0b498118f23f9ad4dadedc4
Amir Bahadori <pbahadori156@gmail.com>
2016-09-13 19:49:41 -0700

    Added the search bar functionality

--- Pokedex3/ViewController.swift
+++ Pokedex3/ViewController.swift
@@ -9,16 +9,23 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var pokemonArray = [Pokemon]()
+    var filteredPokemonArray = [Pokemon]()
+    var inSearchMode = false
     var musicPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        searchBar.delegate = self
+        
+        //This turns the return key to done
+        searchBar.returnKeyType = UIReturnKeyType.done
         
         parsePokemonCSV()
         initAudio()
@@ -65,7 +72,34 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
 
-
+    //SEARCH BAR METHOD - To end the keyboard when we're finished searching for the pokemon
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    //Whenever you type in the search bar, every keystroke we make, we are filtering to see if what we are typing for exists
+    //We need a method for whenever a keystroke is made
+    //SEARCH BAR METHOD
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //When we aren't searching for anything, we want it to show the full list of Pokemon
+        //When we search, we want it to show a different  group of pokemon
+        if searchBar.text == nil || searchBar.text == "" {
+            inSearchMode = false
+            collectionView.reloadData()
+            view.endEditing(true)
+        } else {
+            inSearchMode = true
+            //Take the string in the search bar and compare it with all of the pokemon object names. If what we have in the search bar is included, then we put it in the new filtered pokemon array and siplay that one
+            let lower = searchBar.text!.lowercased()
+            
+            //Here we say the filtered pokemon list is going to be equal to the original pokemon array which is being filtered by using a $0 which is a placeholder for any or all the objects in the original pokemon array 
+            // We're saying each pokemon object, which is our placeholder for our $0, we're taking the name value of that and we're saying "is what we typed in the search bar contained inside of that name and if it is, then we're going to put that object in the filtered pokemon list
+            filteredPokemonArray = pokemonArray.filter({$0.name.range(of: lower) != nil})
+            
+            collectionView.reloadData()
+        }
+    }
+    
   
     //COLLECTIONVIEW REQUIRED METHODS
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -73,8 +107,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
             
             //We show the pokemon we have in our array at that indexPath.row and then pass the Poke into the configureCell function to assign it's name and thumbImg
-            let poke = pokemonArray[indexPath.row]
-            cell.configureCell(poke)
+            let poke: Pokemon!
+            
+            if inSearchMode {
+                poke = filteredPokemonArray[indexPath.row]
+                cell.configureCell(poke)
+            } else {
+                poke = pokemonArray[indexPath.row]
+                cell.configureCell(poke)
+            }
+            
+            
             
             return cell
         } else {
@@ -88,6 +131,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     //This returns how many objects in the Collection View
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        if inSearchMode {
+            return filteredPokemonArray.count
+        }
+        
         return pokemonArray.count
     }
     
